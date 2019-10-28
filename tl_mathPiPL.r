26d9b1a35d3e1d73b0e2a9ed7845934e023dfbf9
crazylafo <31342452+crazylafo@users.noreply.github.com>
2019-10-28 22:05:29 +0100

    disable widetimeinput flags for optimization
    conflict with seqP update

--- tl_mathPiPL.r
+++ tl_mathPiPL.r
@@ -52,11 +52,11 @@ resource 'PiPL' (16000) {
         },
         /* [10] */
         AE_Effect_Global_OutFlags {
-            0x06008006 //50332160
+            0x06008004 //50332160
             
         },
         AE_Effect_Global_OutFlags_2 {
-            0x02140b
+            0x0140b
         },
         /* [11] */
         AE_Effect_Match_Name {
