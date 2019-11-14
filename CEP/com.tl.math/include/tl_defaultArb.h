a9c95d04b7fdd91220addafc379cda1b02e4060f
crazylafo <31342452+crazylafo@users.noreply.github.com>
2019-11-14 18:34:05 +0100

    switch static to reinterpret cast for macos
    for macos

---
+++ CEP/com.tl.math/include/tl_defaultArb.h
@@ -0,0 +1,382 @@
+//
+//  tl_defaultArb.h
+//  tlMath
+//
+//
+
+#ifndef tl_defaultArb_h
+#define tl_defaultArb_h
+
+static std::string defaultArb = R"=====(
+{
+    "effectInfo":{
+        "effectName":"tlMath",
+        "pluginVersion":115,
+        "minimalPluginVersion":115,
+        "tags":["default", "glsl", "expr","skeleton"],
+        "presetName":"default_preset",
+        "description":"simple skeleton_effect"
+    },
+    "effectMode":{
+        "gl33_modeB":true,
+        "expr_modeB":false,
+        "evalModeB":false
+    },
+    "gl_expression":{
+       "gl33_frag_sh":"#version 330 // glsls version for opengl 3.3\\nuniform sampler2D texture0; //call the layer source\\nuniform float slider_1;// call somes variables from the ui\\nuniform float multiplier16bit; //proper to AE 16 bits depth.\\nin vec4 out_pos;\\nin vec2 out_uvs;\\nout vec4 fragColorOut;\\n// to use instead of texture(sampler2D, vec2 uv) because of swizzle RGBA/ ARGBs\\nvec4 loadTextureFromAE (sampler2D tex2d)\\n{\\n    vec2 uv_AE = out_uvs;\\n    uv_AE.y = 1.- out_uvs.y;\\n    vec4 textureIn = texture( tex2d, uv_AE);\\n    textureIn =  textureIn * multiplier16bit;\\n    textureIn= vec4( textureIn.g,  textureIn.b,  textureIn.a,  textureIn.r);\\n    textureIn= vec4( textureIn.a *  textureIn.r,  textureIn.a *  textureIn.g,  textureIn.a * textureIn.b,  textureIn.a);\\n    return  textureIn ;\\n}\\nvec4 loadTextureOffset (sampler2D tex2d,  vec2 off){\\n    vec2 uv_AE = out_uvs;\\n    uv_AE.x = out_uvs.x +off.x;\\n    uv_AE.y = 1.- (out_uvs.y+off.y);\\n    vec4 textureIn = texture( tex2d, uv_AE);\\n    textureIn =  textureIn * multiplier16bit;\\n    textureIn= vec4( textureIn.g,  textureIn.b,  textureIn.a,  textureIn.r);\\n    textureIn= vec4( textureIn.a *  textureIn.r,  textureIn.a *  textureIn.g,  textureIn.a * textureIn.b,  textureIn.a);\\n    return  textureIn ;\\n\\n}\\n\\nvoid main(void)\\n{\\n    fragColorOut= loadTextureFromAE(texture0);\\n    fragColorOut.r *=slider_1/100; \\n}",
+        "gl33_vert_sh":"#version 330 \\n in vec4 Position;\\nin vec2 UVs;\\nout vec4 out_pos;\\nout vec2 out_uvs;\\nuniform mat4 ModelviewProjection;\\nvoid main(void)\\n{\\nout_pos = ModelviewProjection * Position; \\n gl_Position = out_pos; \\nout_uvs = UVs;\\n}",
+        "gl33_frag_error" :  "compiled successfully",
+        "gl33_vert_error" :  "compiled successfully"
+    },
+    "math_expression":{
+        "redExpr":"return inChannel;",
+        "greenExpr":"return inChannel;",
+        "blueExpr":"return inChannel;",
+        "rgbExpr":"return inChannel;",
+        "alphaExpr":"return inChannel;",
+        "red_error":  "compiled successfully",
+        "green_error" :  "compiled successfully",
+        "blue_error" :  "compiled successfully",
+        "alpha_error" :  "compiled successfully",
+        "rgb_error" :  "compiled successfully",
+        "exprRGBModeB":true,
+        "expr_current_channel":"inChannel",
+        "expr_pix":"pix"
+        },
+    "flags":{
+        "pixelsCallExternalInputB":[false,false,false,false],
+        "needsLumaB":false,
+        "presetHasWideInputB":false,
+        "usesCameraB":false
+    },
+    "composition":{
+        "resolution":"resolution",
+        "layerPosition": "layerPosition",
+        "layerScale":"layerScale",
+        "compResolution": "compResolution",
+        "time_sec":"time",
+        "time_frame":"timef",
+        "frame_rate":"fps",
+        "camera_position":"camera_pos",
+        "camera_target":"camera_target",
+        "camera_zoom":"camera_zoom",
+        "camera_rotation": "camera_rotation"
+    },
+    "gui_settings":{
+        "sliderGrp":{
+            "grpVisibleB":true,
+            "grpName":"Slider Group",
+            "params":[
+                      {
+                          "visibleB":true,
+                          "name":"slider_1",
+                          "defaultVal":[0]
+                      },
+                      {
+                          "visibleB":true,
+                          "name":"slider_2",
+                          "defaultVal":[0]
+                      },
+                      {
+                          "visibleB":true,
+                          "name":"slider_3",
+                          "defaultVal":[0]
+                      },
+                      {
+                          "visibleB":true,
+                          "name":"slider_4",
+                          "defaultVal":[0]
+                      },
+                      {
+                          "visibleB":true,
+                          "name":"slider_5",
+                          "defaultVal":[0]
+                      },
+                      {
+                          "visibleB":true,
+                          "name":"slider_6",
+                          "defaultVal":[0]
+                      },
+                      {
+                          "visibleB":true,
+                          "name":"slider_7",
+                          "defaultVal":[0]
+                      },
+                      {
+                          "visibleB":true,
+                          "name":"slider_8",
+                          "defaultVal":[0]
+                      },
+                      {
+                          "visibleB":true,
+                          "name":"slider_9",
+                          "defaultVal":[0]
+                      },
+                      {
+                          "visibleB":true,
+                          "name":"slider_10",
+                          "defaultVal":[0]
+                      }
+                      ]
+        },
+        "pointGrp":{
+            "grpVisibleB":true,
+            "grpName":"3d Point Group",
+            "params": [
+                       {
+                           "visibleB":true,
+                           "name":"point_1",
+                           "defaultVal":[50,50,50]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"point_2",
+                           "defaultVal":[50,50,50]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"point_3",
+                           "defaultVal":[50,50,50]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"point_4",
+                           "defaultVal":[50,50,50]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"point_5",
+                           "defaultVal":[50,50,50]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"point_6",
+                           "defaultVal":[50,50,50]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"point_7",
+                           "defaultVal":[50,50,50]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"point_8",
+                           "defaultVal":[50,50,50]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"point_9",
+                           "defaultVal":[50,50,50]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"point_10",
+                           "defaultVal":[50,50,50]
+                       }
+                       ]
+        },
+        "colorGrp":{
+            "grpVisibleB":true,
+            "grpName":"3d color Group",
+            "params": [
+                       {
+                           "visibleB":true,
+                           "name":"color_1",
+                           "defaultVal":[1.0,1.0,1.0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"color_2",
+                           "defaultVal":[1.0,1.0,1.0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"color_3",
+                           "defaultVal":[1.0,1.0,1.0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"color_4",
+                           "defaultVal":[1.0,1.0,1.0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"color_5",
+                           "defaultVal":[1.0,1.0,1.0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"color_6",
+                           "defaultVal":[1.0,1.0,1.0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"color_7",
+                           "defaultVal":[1.0,1.0,1.0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"color_8",
+                           "defaultVal":[1.0,1.0,1.0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"color_9",
+                           "defaultVal":[1.0,1.0,1.0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"color_10",
+                           "defaultVal":[1.0,1.0,1.0]
+                       }
+                       ]
+        },
+        "cboxGrp":{
+            "grpVisibleB":true,
+            "grpName":"check box Group",
+            "params": [
+                       {
+                           "visibleB":true,
+                           "name":"cbox_1",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"cbox_2",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"cbox_3",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"cbox_4",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"cbox_5",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"cbox_6",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"cbox_7",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"cbox_8",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"cbox_9",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"cbox_10",
+                           "defaultVal":[0]
+                       }
+                       ]
+        },
+        "rotationGrp":{
+            "grpVisibleB":true,
+            "grpName":"rotation Group",
+            "params": [
+                       {
+                           "visibleB":true,
+                           "name":"rotation_1",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"rotation_2",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"rotation_3",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"rotation_4",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"rotation_5",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"rotation_6",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"rotation_7",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"rotation_8",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"rotation_9",
+                           "defaultVal":[0]
+                       },
+                       {
+                           "visibleB":true,
+                           "name":"rotation_10",
+                           "defaultVal":[0]
+                       }
+                       ]
+        },
+        "layerGrp":{
+            "grpVisibleB":true,
+            "grpName":"Layer Group",
+            "current_layer":{
+                "name":"texture0"
+            },
+            "extLayer_1":{
+                "visibleB":true,
+                "name":"texture1",
+                "poffName":"layer1_position_off",
+                "toffName":"layer1_time_off"
+            },
+            "extLayer_2":{  
+                "visibleB":true,
+                "name":"texture2",
+                "poffName":"layer2_position_off",
+                "toffName":"layer2_time_off"
+            },
+            "extLayer_3":{
+                "visibleB":true,
+                "name":"texture3",
+                "poffName":"layer3_position_off",
+                "toffName":"layer3_time_off"
+            },
+            "extLayer_4":{
+                "visibleB":true,
+                "name":"texture4",
+                "poffName":"layer4_position_off",
+                "toffName":"layer4_time_off"
+            }
+        }
+    }
+}
+)=====";
+
+
+#endif /* tl_defaultArb_h */
---
+++ CEP/com.tl.math/src/tl_math_Strings.cpp
@@ -0,0 +1,107 @@
+
+#include "tl_math.h"
+
+typedef struct {
+	A_u_long	index;
+	A_char		str[256];
+} TableString;
+
+
+
+
+
+TableString		g_strs[StrID_NUMTYPES] = {
+	StrID_NONE,						    "",
+	StrID_Name,						    "tl Maths",
+	StrID_Description,				    "Execute GLSL shaders or Math operations in the pixel world.",
+	STR_ID_BUTTON_SETUP_Param_Name,     "SETUP",
+    StrID_EffectDescription,            "Description",
+    STR_ID_MATH_TOPIC_POINTS_Param_Name,   "slider_group",
+    STR_ID_MATH_SLIDER_ONE_Param_Name,      "slider01",
+    STR_ID_MATH_SLIDER_TWO_Param_Name,      "slider02",
+    STR_ID_MATH_SLIDER_THREE_Param_Name,    "slider03",
+    STR_ID_MATH_SLIDER_FOUR_Param_Name,     "slider04",
+    STR_ID_MATH_SLIDER_FIVE_Param_Name,     "slider05",
+    STR_ID_MATH_SLIDER_SIX_Param_Name,      "slider06",
+    STR_ID_MATH_SLIDER_SEVEN_Param_Name,    "slider07",
+    STR_ID_MATH_SLIDER_HEIGHT_Param_Name,   "slider08",
+    STR_ID_MATH_SLIDER_NINE_Param_Name,     "slider09",
+    STR_ID_MATH_SLIDER_TEN_Param_Name,      "slider10",
+    STR_ID_MATH_TOPIC_POINTS_Param_Name,    "point_group",
+    STR_ID_MATH_POINT_ONE_Param_Name,       "point01",
+    STR_ID_MATH_POINT_TWO_Param_Name,       "point02",
+    STR_ID_MATH_POINT_THREE_Param_Name,     "point03",
+    STR_ID_MATH_POINT_FOUR_Param_Name,      "point04",
+    STR_ID_MATH_POINT_FIVE_Param_Name,       "point05",
+    STR_ID_MATH_POINT_SIX_Param_Name,       "point06",
+    STR_ID_MATH_POINT_SEVEN_Param_Name,     "point07",
+    STR_ID_MATH_POINT_HEIGHT_Param_Name,     "point08",
+    STR_ID_MATH_POINT_NINE_Param_Name,      "point09",
+    STR_ID_MATH_POINT_TEN_Param_Name,       "point10",
+    STR_ID_MATH_TOPIC_CB_Param_Name,        "checkbox_group",
+    STR_ID_MATH_CB_ONE_Param_Name,          "cb01",
+    STR_ID_MATH_CB_TWO_Param_Name,          "cb02",
+    STR_ID_MATH_CB_THREE_Param_Name,        "cb03",
+    STR_ID_MATH_CB_FOUR_Param_Name,         "cb04",
+    STR_ID_MATH_CB_FIVE_Param_Name,         "cb05",
+    STR_ID_MATH_CB_SIX_Param_Name,          "cb06",
+    STR_ID_MATH_CB_SEVEN_Param_Name,        "cb07",
+    STR_ID_MATH_CB_HEIGHT_Param_Name,       "cb08",
+    STR_ID_MATH_CB_NINE_Param_Name,         "cb09",
+    STR_ID_MATH_CB_TEN_Param_Name,           "cb10",
+    STR_ID_MATH_TOPIC_COLORS_Param_Name,      "color_group",
+    STR_ID_MATH_COLOR_ONE_Param_Name,       "color01",
+    STR_ID_MATH_COLOR_TWO_Param_Name,       "color02",
+    STR_ID_MATH_COLOR_THREE_Param_Name,      "color03",
+    STR_ID_MATH_COLOR_FOUR_Param_Name,      "color04",
+    STR_ID_MATH_COLOR_FIVE_Param_Name,      "color05",
+    STR_ID_MATH_COLOR_SIX_Param_Name,       "color06",
+    STR_ID_MATH_COLOR_SEVEN_Param_Name,      "color07",
+    STR_ID_MATH_COLOR_HEIGHT_Param_Name,     "color08",
+    STR_ID_MATH_COLOR_NINE_Param_Name,      "color09",
+    STR_ID_MATH_COLOR_TEN_Param_Name,       "color10",
+	STR_ID_MATH_TOPIC_ROTS_Param_Name,     "rotation_group",
+	STR_ID_MATH_ROT_ONE_Param_Name,         "rotation01",
+	STR_ID_MATH_ROT_TWO_Param_Name,         "rotation02",
+	STR_ID_MATH_ROT_THREE_Param_Name,		"rotation03",
+	STR_ID_MATH_ROT_FOUR_Param_Name,		"rotation04",
+	STR_ID_MATH_ROT_FIVE_Param_Name,		"rotation05",
+	STR_ID_MATH_ROT_SIX_Param_Name,			"rotation06",
+	STR_ID_MATH_ROT_SEVEN_Param_Name,		"rotation07",
+	STR_ID_MATH_ROT_HEIGHT_Param_Name,		"rotation08",
+	STR_ID_MATH_ROT_NINE_Param_Name,		"rotation09",
+	STR_ID_MATH_ROT_TEN_Param_Name,			"rotation10",
+
+    StrID_TOPIC_INPUTS_Param_Name,      "Inputs",
+    StrID_LAYER_ONE_Param_Name,         "Layer1 input",
+    StrID_TOFF_ONE_Param_Name,          "layer1 Frames Offset",
+    StrID_POFF_ONE_Param_Name,          "Shift Layer1 Center to",
+
+	StrID_LAYER_TWO_Param_Name,         "Layer2 input",
+	StrID_TOFF_TWO_Param_Name,          "layer2 Frames Offset",
+	StrID_POFF_TWO_Param_Name,          "Shift Layer2 Center to",
+
+	StrID_LAYER_THREE_Param_Name,         "Layer3 input",
+	StrID_TOFF_THREE_Param_Name,          "layer3 Frames Offset",
+	StrID_POFF_THREE_Param_Name,          "Shift Layer3 Center to",
+
+	StrID_LAYER_FOUR_Param_Name,         "Layer4 input",
+	StrID_TOFF_FOUR_Param_Name,          "layer4 Frames Offset",
+	StrID_POFF_FOUR_Param_Name,          "Shift Layer4 Center to",
+
+	StrID_CEP_GETARB_Param_Name,         "get arb",
+    StrID_CEP_REturnMessage_Param_Name,  "arb received",
+    StrID_VersionError,                 "Error : your plugin version is lower than the preset",
+    StrID_JSONError,                   "Error: error in JSON data reading"
+
+
+};
+
+
+char	*GetStringPtr(int strNum)
+{
+	return g_strs[strNum].str;
+}
+
+
+
